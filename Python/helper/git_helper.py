e2fea18600dcbfd1f31308f6ecec92b1aef32462
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-09-14 12:02:33 +0800

    made some refactor

--- Python/helper/git_helper.py
+++ Python/helper/git_helper.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import sys
 
 def colorize(color:str) -> str:
     if color == "red":
@@ -21,7 +22,34 @@ def colorize(color:str) -> str:
     else:
         return ""
 
-parser = argparse.ArgumentParser(description="Push to all git remotes with one command")
+def get_repo_http_url(remote):
+    url =  subprocess.check_output(f"git -C {git_path} remote get-url {remote}", shell=True).decode("utf-8").strip()
+    if url.startswith("git@"):
+        url = url.replace(":", "/")
+        url = url.replace("git@", "https://")
+        url = url.replace(".git", "")
+    return url
+
+def commit():
+    commit_message = " ".join(argv.commit)
+    subprocess.call(f"git -C {git_path} add .", shell=True)
+    subprocess.call(f"git -C {git_path} commit -m \"{commit_message}\"", shell=True)
+
+def push():
+    for remote in remote_name:
+        print(f"Pushing to remote {colorize('green')}{remote}{colorize('reset')}")
+        print(f"{colorize('yellow')}")
+        if argv.branch is None:
+            subprocess.call(f"git -C {git_path} push {remote} {current_branch}", shell=True)
+        else:
+            subprocess.call(f"git -C {git_path} push {remote} {argv.branch}", shell=True)
+        print(f"{colorize('reset')}")
+        print(f"Pushed to {colorize('green')}{get_repo_http_url(remote)}{colorize('reset')}\n")
+        print(f"{colorize('reset')}{'-'*15}{colorize('reset')}")
+
+    
+
+parser = argparse.ArgumentParser(description="Push to all git remotes with one command" )
 parser.add_argument("-p", "--push", help="Push to all remotes", action="store_true")
 parser.add_argument("-s", "--status", help="Show the status of all remotes", action="store_true")
 parser.add_argument("-b", "--branch", help="Specify the branch to push to. (Default current branch)", type=str)
@@ -29,7 +57,8 @@ parser.add_argument("-l", "--list", help="List out remote / branch", type=str , 
 parser.add_argument("-c" , "--commit" , help="Commit all changes" , type=str , nargs="+" )
 parser.add_argument("-cp" , "--commit_push" , help="Commit all changes and push to all remote" , type=str , nargs="+" )
 
-argv = parser.parse_args()
+
+argv = parser.parse_args( args=None if sys.argv[1:] else ['--help'])
 
 remote_name = subprocess.check_output("git remote", shell=True).decode("utf-8").split("\n")
 remote_name.pop() # remove the last empty element
@@ -51,14 +80,7 @@ if argv.status is True:
     exit()
 
 if argv.push is True:
-    for remote in remote_name:
-        print(f"Pushing to {colorize('green')}{remote}{colorize('reset')}")
-        if argv.branch is None:
-            subprocess.call(f"git -C {git_path} push {remote} {current_branch}", shell=True)
-        else:
-            subprocess.call(f"git -C {git_path} push {remote} {argv.branch}", shell=True)
-        print()
-    
+    push()
     exit()
 
 if argv.list is not None:
@@ -71,24 +93,12 @@ if argv.list is not None:
 
     exit()
 
+
 if argv.commit is not None:
-    commit_message = " ".join(argv.commit)
-    subprocess.call(f"git -C {git_path} add .", shell=True)
-    subprocess.call(f"git -C {git_path} commit -m \"{commit_message}\"", shell=True)
+    commit()
     exit()
 
 if argv.commit_push is not None:
-    commit_message = " ".join(argv.commit_push)
-    subprocess.call(f"git -C {git_path} add .", shell=True)
-    subprocess.call(f"git -C {git_path} commit -m \"{commit_message}\"", shell=True)
-    for remote in remote_name:
-        print(f"Pushing to {colorize('green')}{remote}{colorize('reset')}")
-        if argv.branch is None:
-            print (f"git -C {git_path} push {remote} {current_branch}")
-            # subprocess.call(f"git -C {git_path} push {remote} {current_branch}", shell=True)
-        else:
-            print (f"git -C {git_path} push {remote} {argv.branch}")
-            # subprocess.call(f"git -C {git_path} push {remote} {argv.branch}", shell=True)
-        print()
-    
+    commit()
+    push()
     exit()
